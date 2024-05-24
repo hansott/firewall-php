0a0c9a080a1f71a6b5debfdf826564e530f1611f
Tudor <tudor@aikido.dev>
2024-05-24 16:31:42 +0300

    Removed username & password from pdo constructor hooking

--- src/extension/HandlePDO.cpp
+++ src/extension/HandlePDO.cpp
@@ -6,15 +6,9 @@ ZEND_NAMED_FUNCTION(handle_pdo___construct) {
 	char *data_source;
 	size_t data_source_len;
 	char *colon;
-	char *username=NULL, *password=NULL;
-	size_t usernamelen, passwordlen;
 
-	ZEND_PARSE_PARAMETERS_START(1, 4)
+	ZEND_PARSE_PARAMETERS_START(1, -1)
 		Z_PARAM_STRING(data_source, data_source_len)
-		Z_PARAM_OPTIONAL
-		Z_PARAM_STRING_OR_NULL(username, usernamelen)
-		Z_PARAM_STRING_OR_NULL(password, passwordlen)
-		//Z_PARAM_ARRAY_OR_NULL(options)
 	ZEND_PARSE_PARAMETERS_END();
 
 	std::string data_source_string(data_source, data_source_len);
@@ -29,15 +23,6 @@ ZEND_NAMED_FUNCTION(handle_pdo___construct) {
 			} }
 		} }
 	};
-
-	if (username) {
-		std::string usernameString(username, usernamelen);
-		pdo_construct_event["data"]["parameters"]["username"] = usernameString;
-	}
-	if (password) {
-		std::string passwordString(password, passwordlen);
-		pdo_construct_event["data"]["parameters"]["password"] = passwordString;
-	}
 
 	GoOnEvent(pdo_construct_event);
 
--- src/lib/handle_pdo.go
+++ src/lib/handle_pdo.go
@@ -3,13 +3,13 @@ package main
 import "fmt"
 
 func OnMethodExecutedPdoConstruct(parameters map[string]interface{}) string {
-	data_source := GetFromMap[string](parameters, "data_source")
-	fmt.Println("[AIKIDO-GO] Got PDO data_source:", *data_source)
+	data_source := MustGetFromMap[string](parameters, "data_source")
+	fmt.Println("[AIKIDO-GO] Got PDO data_source:", data_source)
 	return "{}"
 }
 
 func OnMethodExecutedPdoQuery(parameters map[string]interface{}) string {
-	query := GetFromMap[string](parameters, "query")
-	fmt.Println("[AIKIDO-GO] Got PDO query:", *query)
+	query := MustGetFromMap[string](parameters, "query")
+	fmt.Println("[AIKIDO-GO] Got PDO query:", query)
 	return "{}"
 }
