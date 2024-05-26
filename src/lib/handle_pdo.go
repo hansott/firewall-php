cf55421369a9399f5a58d352ffbb247109b4fd47
Tudor <tudor@aikido.dev>
2024-05-26 11:30:09 +0300

    Cleanup

--- src/lib/handle_pdo.go
+++ src/lib/handle_pdo.go
@@ -1,15 +1,11 @@
 package main
 
-import "fmt"
-
 func OnMethodExecutedPdoConstruct(parameters map[string]interface{}) string {
-	data_source := MustGetFromMap[string](parameters, "data_source")
-	fmt.Println("[AIKIDO-GO] Got PDO data_source:", data_source)
+	//data_source := MustGetFromMap[string](parameters, "data_source")
 	return "{}"
 }
 
 func OnMethodExecutedPdoQuery(parameters map[string]interface{}) string {
-	query := MustGetFromMap[string](parameters, "query")
-	fmt.Println("[AIKIDO-GO] Got PDO query:", query)
+	//query := MustGetFromMap[string](parameters, "query")
 	return "{}"
 }
