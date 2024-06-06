3b453c5f2cef5e5e71464886ef638294d6369f70
Tudor <tudor@aikido.dev>
2024-06-06 11:34:19 -0400

    Build support extended for gRPC

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 rm -rf build
 mkdir build
 cd src/agent
+go get google.golang.org/grpc
 go build -o ../../build/aikido
 cd ../extension
 phpize
 cd ../lib
+go get google.golang.org/grpc
 go build -buildmode=c-archive -o ../../build/libaikido_go.a
 cd ../../build
 CXX=g++ CXXFLAGS="-fPIC -std=c++20 -g -O0 -I../include" LDFLAGS="-L./ -laikido_go" ../src/extension/configure
