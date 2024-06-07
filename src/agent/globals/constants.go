eee224b7ed99a37a46112edfb60e11ae713d8760
Tudor <tudor@aikido.dev>
2024-06-07 04:26:33 -0400

    Change version to 0.2.0 and update release workflow

--- src/agent/globals/constants.go
+++ src/agent/globals/constants.go
@@ -1,7 +1,7 @@
 package globals
 
 const (
-	Version               = "0.1.0"
+	Version               = "0.2.0"
 	ConfigFilePath        = "/opt/aikido/config.json"
 	SocketPath            = "/var/aikido.sock"
 	ConfigAPIMethod       = "GET"
