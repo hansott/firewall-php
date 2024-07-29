dd175b8d27b4337ae3743f509296b44b3d6b5b3b
Tudor <tudor@aikido.dev>
2024-07-29 15:05:57 +0300

    Remove version where not necessary

--- rpm_build.sh
+++ rpm_build.sh
@@ -1,7 +1,7 @@
 rm -rf ~/rpmbuild
 rpmdev-setuptree
 
-VERSION="1.0.29"
+VERSION=$(grep '# define PHP_AIKIDO_VERSION' include/php_aikido.h | awk -F'"' '{print $2}')
 
 mkdir -p ~/rpmbuild/SOURCES/aikido-php-firewall-$VERSION
 cp -rf package/rpm/opt ~/rpmbuild/SOURCES/aikido-php-firewall-$VERSION/
