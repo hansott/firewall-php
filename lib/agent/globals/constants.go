f3fc501fda61c03b11cbd0ad6ef28b2cf25b9fea
Tudor <tudor@aikido.dev>
2024-08-27 15:02:02 +0300

    1.0.57

--- lib/agent/globals/constants.go
+++ lib/agent/globals/constants.go
@@ -1,7 +1,7 @@
 package globals
 
 const (
-	Version                     = "1.0.56"
+	Version                     = "1.0.57"
 	SocketPath                  = "/run/aikido-" + Version + ".sock"
 	ConfigUpdatedAtMethod       = "GET"
 	ConfigUpdatedAtAPI          = "/config"
