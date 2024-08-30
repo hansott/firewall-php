f77ff2a785ca68349eca8dd643401aced32fa056
Tudor <tudor@aikido.dev>
2024-08-30 13:56:14 +0300

    test

---
+++ tests/server/test_bypassed_ip_for_rate_limiting/test.py
@@ -0,0 +1,39 @@
+import requests
+import time
+import sys
+from testlib import *
+
+'''
+1. Sets up the bypassed IP address config for route '/test'. Rate limiting is set to 10 req / min. Checks that requests are not rate limited blocked.
+2. Changes the config to remove the bypassed IP address. Checks that requests are rate limiting.
+3. Changes the config again to enable the bypassed IP address. Checks that requests are not rate limited blocked.
+'''
+
+
+def run_test(php_port, mock_port):
+    for _ in range(100):
+        response = php_server_get(php_port, "/")
+        assert_response_code_is(response, 200)
+        
+    mock_server_set_config_file(mock_port, "change_config_remove_bypassed_ip.json")
+    time.sleep(120)
+
+    for i in range(100):
+        response = php_server_get(php_port, "/")
+        if i < 10:
+            assert_response_code_is(response, 200)
+        else:
+            assert_response_code_is(response, 429)
+            assert_reponse_header_contains(response, "Content-Type", "text")
+            assert_reponse_body_contains(response, "This request was rate limited by Aikido Security!")
+
+    mock_server_set_config_file(mock_port, "start_config.json")
+    time.sleep(120)  
+    
+    for _ in range(100):
+        response = php_server_get(php_port, "/test")
+        assert_response_code_is(response, 200)
+    
+    
+if __name__ == "__main__":
+    run_test(int(sys.argv[1]), int(sys.argv[2]))
