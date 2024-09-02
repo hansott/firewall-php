22207eb3d1461ad93bfa754357692fbf5b5103d3
Tudor <tudor@aikido.dev>
2024-09-02 16:18:53 +0300

    more tests

---
+++ tests/server/test_routes/test.py
@@ -0,0 +1,42 @@
+import requests
+import time
+import sys
+from testlib import *
+
+'''
+1. Sets up a simple config.
+2. Sends multiple requests to different routes.
+3. Waits for the heartbeat event and validates the reporting.
+'''
+
+routes = {
+    "/",
+    "/test",
+    "/api/info/12345",
+    "/api/info/12345678",
+    "/api/secret/CnJ4DunhYfv2db6T1FRfciRBHtlNKOYrjoz",
+    "/api/secret/hIofuWBifkJI5iVsSNKKKDpBfmMqJJwuXMxau6AS8WZaHVLDAMeJXo3BwsFyrIIm",
+    "/api/uuid/2aa11254-7c5a-4427-b819-a85063b157ca",
+    "/api/uuid/52f80bbb-a3aa-44bc-b980-e51db0785fe3",
+    "/api/uuid/6d380fc0-143e-4c48-b456-82ac4592fe06",
+    "/posts/2023-05-01",
+    "/posts/2024-05-01",
+    "/posts/2022-03-28",
+    "/posts/1994-09-01",
+}
+
+def run_test(php_port, mock_port):
+    for route in routes:
+        for nr_requests in range(10):
+            response = php_server_get(php_port, route)
+            assert_response_code_is(response, 200)
+    
+    mock_server_wait_for_new_events(mock_port, 70)
+    
+    events = mock_server_get_events(mock_port)
+    assert_events_length_is(events, 2)
+    assert_started_event_is_valid(events[0])
+    assert_event_contains_subset_file(events[1], "expect_routes.json")
+    
+if __name__ == "__main__":
+    run_test(int(sys.argv[1]), int(sys.argv[2]))
