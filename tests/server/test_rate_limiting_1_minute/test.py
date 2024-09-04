f58c97e53fc21d30c4533ea3f77faf5687918fc9
Tudor <tudor@aikido.dev>
2024-09-04 20:07:13 +0300

    sleep

--- tests/server/test_rate_limiting_1_minute/test.py
+++ tests/server/test_rate_limiting_1_minute/test.py
@@ -15,7 +15,7 @@ def run_test(php_port, mock_port):
         response = php_server_get(php_port, "/")
         assert_response_code_is(response, 200)
         
-    time.sleep(0.5)
+    time.sleep(10)
         
     for _ in range(5):
         response = php_server_get(php_port, "/")
