924bf7d96393e5acc08319dd2f3aa5b4cc5ac7d1
Tudor <tudor@aikido.dev>
2024-09-05 11:21:58 +0300

    1.0.62

--- lib/agent/globals/constants.go
+++ lib/agent/globals/constants.go
@@ -1,7 +1,7 @@
 package globals
 
 const (
-	Version                            = "1.0.61"
+	Version                            = "1.0.62"
 	ConfigUpdatedAtMethod              = "GET"
 	ConfigUpdatedAtAPI                 = "/config"
 	ConfigAPIMethod                    = "GET"
--- package/rpm/aikido.spec
+++ package/rpm/aikido.spec
@@ -1,5 +1,5 @@
 Name:           aikido-php-firewall
-Version:        1.0.61
+Version:        1.0.62
 Release:        1
 Summary:        Aikido PHP Extension
 License:        GPL
