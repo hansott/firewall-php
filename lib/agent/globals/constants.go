0cde8f3f3cc2316b0a121ccaaa1b1a24910113e9
Tudor <tudor@aikido.dev>
2024-09-06 18:09:59 +0300

    debug

--- lib/agent/globals/constants.go
+++ lib/agent/globals/constants.go
@@ -1,7 +1,7 @@
 package globals
 
 const (
-	Version                            = "1.0.67"
+	Version                            = "1.0.68"
 	ConfigUpdatedAtMethod              = "GET"
 	ConfigUpdatedAtAPI                 = "/config"
 	ConfigAPIMethod                    = "GET"
--- lib/php-extension/include/php_aikido.h
+++ lib/php-extension/include/php_aikido.h
@@ -6,7 +6,7 @@
 extern zend_module_entry aikido_module_entry;
 # define phpext_aikido_ptr &aikido_module_entry
 
-# define PHP_AIKIDO_VERSION "1.0.67"
+# define PHP_AIKIDO_VERSION "1.0.68"
 
 # if defined(ZTS) && defined(COMPILE_DL_AIKIDO)
 ZEND_TSRMLS_CACHE_EXTERN()
