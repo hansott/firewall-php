d3abe65f105cd5f5d47827badf85dd2be774fdd0
Tudor <tudor@aikido.dev>
2024-09-20 12:04:25 +0300

    test

--- tests/benchmarks/cli/benchmark_outgoing_request/php_code_to_test.php
+++ tests/benchmarks/cli/benchmark_outgoing_request/php_code_to_test.php
@@ -1,7 +1,7 @@
 <?php
     $ch1 = curl_init("https://example.com/");
     curl_setopt($ch1, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch1, CURLOPT_TIMEOUT_MS, 10);
+    curl_setopt($ch1, CURLOPT_TIMEOUT_MS, 1);
     curl_exec($ch1);
     curl_close($ch1);
 ?>
