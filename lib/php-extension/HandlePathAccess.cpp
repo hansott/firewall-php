a57cdf3f0caf3a33c6b0132183e203d887770743
Tudor <tudor@aikido.dev>
2024-09-25 14:49:07 +0300

    Path access parsing improvements and remove harmless functions

--- lib/php-extension/HandlePathAccess.cpp
+++ lib/php-extension/HandlePathAccess.cpp
@@ -1,9 +1,26 @@
-#include "HandlePathTraversal.h"
+#include "HandlePathAccess.h"
 #include "Utils.h"
 #include "Cache.h"
 
 /* Handles PHP functions that have a file path as first parameter */
 AIKIDO_HANDLER_FUNCTION(handle_file_path_access) {
+    zend_string *filename = NULL;
+
+    ZEND_PARSE_PARAMETERS_START(0, -1)
+        Z_PARAM_OPTIONAL
+        Z_PARAM_STR(filename)
+    ZEND_PARSE_PARAMETERS_END();
+
+    if (!filename) {
+        return;
+    }
+
+    eventId = EVENT_PRE_PATH_ACCESSED;
+    eventCache.filename = ZSTR_VAL(filename);
+}
+
+/* Handles PHP functions that have a file path as both first and second parameter */
+AIKIDO_HANDLER_FUNCTION(handle_file_path_access_2) {
     zend_string *filename = NULL;
     zend_string *filename2 = NULL;
 
@@ -17,9 +34,10 @@ AIKIDO_HANDLER_FUNCTION(handle_file_path_access) {
         return;
     }
 
+    eventId = EVENT_PRE_PATH_ACCESSED;
     eventCache.filename = ZSTR_VAL(filename);
-    if (filename2) {
+    if (filename2)
+    {
         eventCache.filename2 = ZSTR_VAL(filename2);
     }
-    eventId = EVENT_PRE_PATH_ACCESSED;
 }
