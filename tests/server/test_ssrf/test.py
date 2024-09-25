b9a11faff0218252e409e91fee3fb8b521899079
Tudor <tudor@aikido.dev>
2024-09-25 23:00:25 +0300

    SSRF E2E test + clear some benchmarks that we don't need

---
+++ tests/server/test_ssrf/test.py
@@ -0,0 +1,35 @@
+import requests
+import time
+import sys
+from testlib import *
+
+'''
+1. Sets up a simple config and env AIKIDO_BLOCKING=1.
+2. Sends an attack request to a route, that will cause sending a detection event.
+3. Checks that the detection event was submitted and is valid.
+'''
+
+def check_ssrf(response_code, response_body, event_id, expected_json):
+    response = php_server_post("/testDetection", {"url": "http://127.0.0.1:8081"})
+    assert_response_code_is(response, response_code)
+    assert_response_body_contains(response, response_body)
+    
+    mock_server_wait_for_new_events(5)
+    
+    events = mock_server_get_events()
+    assert_events_length_is(events, event_id + 1)
+    assert_started_event_is_valid(events[0])
+    assert_event_contains_subset_file(events[event_id], expected_json)
+
+def run_test():
+    check_ssrf(500, "", 1, "expect_detection_blocked.json")
+    
+    apply_config("change_config_disable_blocking.json")
+    check_ssrf(200, "Got URL content!", 2, "expect_detection_not_blocked.json")
+    
+    apply_config("start_config.json")
+    check_ssrf(500, "", 3, "expect_detection_blocked.json")
+    
+if __name__ == "__main__":
+    load_test_args()
+    run_test()
