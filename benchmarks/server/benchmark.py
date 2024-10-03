cb5d429b697792afd4b8657e8c24ed8970ebee5c
Tudor <tudor@aikido.dev>
2024-10-03 23:34:27 +0300

    benchmark api spec

--- benchmarks/server/benchmark.py
+++ benchmarks/server/benchmark.py
@@ -5,9 +5,12 @@ from testlib import *
 import json
 
 
-def run_benchmark():        
+def run_benchmark():
     for _ in range(10000):
-        response = php_server_post("/test", {}, benchmark=True)
+        response = php_server_post("/test?userId=12345&status=pending&orderId=80", 
+                                   { "orderPlaced": True },
+                                   { "Content-Type": "application/json", "X-API-Key": "abcdef12345" },
+                                   benchmark=True)
         assert_response_code_is(response, 200)
     
 if __name__ == "__main__":
