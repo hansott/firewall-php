2cdb7bd61609b0415f18c70b47bc909149956fd7
Tudor <tudor@aikido.dev>
2024-10-14 21:53:37 +0300

    generic text

--- tests/server/test_allowed_ip/test.py
+++ tests/server/test_allowed_ip/test.py
@@ -14,7 +14,7 @@ def run_test():
     response = php_server_get("/test")
     assert_response_code_is(response, 403)
     assert_response_header_contains(response, "Content-Type", "text")
-    assert_response_body_contains(response, "Your IP address is not allowed to access this resource! (Your IP: ::1)")
+    assert_response_body_contains(response, "Your IP address is not allowed to access this resource! (Your IP: ")
 
     apply_config("change_config_remove_allowed_ip.json")
         
