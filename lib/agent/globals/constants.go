1ce8c4a3f7d15d5f619e7dc3880cc15aecbe6055
Tudor <tudor@aikido.dev>
2024-10-24 14:51:05 +0300

    v1.0.81

--- lib/agent/globals/constants.go
+++ lib/agent/globals/constants.go
@@ -1,7 +1,7 @@
 package globals
 
 const (
-	Version                            = "1.0.80"
+	Version                            = "1.0.81"
 	ConfigUpdatedAtMethod              = "GET"
 	ConfigUpdatedAtAPI                 = "/config"
 	ConfigAPIMethod                    = "GET"
--- lib/request-processor/globals/globals.go
+++ lib/request-processor/globals/globals.go
@@ -11,5 +11,5 @@ var CloudConfig CloudConfigData
 var CloudConfigMutex sync.Mutex
 
 const (
-	Version = "1.0.80"
+	Version = "1.0.81"
 )
