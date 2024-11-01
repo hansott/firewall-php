b9fcaaa41f3e625abaa38b109b8c7c04037fcf5a
Tudor <tudor@aikido.dev>
2024-11-01 17:52:43 +0200

    Quick update version test

--- lib/agent/globals/constants.go
+++ lib/agent/globals/constants.go
@@ -1,7 +1,7 @@
 package globals
 
 const (
-	Version                            = "1.0.88"
+	Version                            = "1.0.89"
 	ConfigUpdatedAtMethod              = "GET"
 	ConfigUpdatedAtAPI                 = "/config"
 	ConfigAPIMethod                    = "GET"
