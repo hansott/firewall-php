fcb5228e5a31ed08cba6fc1c0c7fa72e7cdcf946
Tudor TIMCU <tudor@aikido.dev>
2024-11-26 10:40:30 +0000

    Remove checking shell inj for proc_open on array

--- lib/php-extension/HandleShellExecution.cpp
+++ lib/php-extension/HandleShellExecution.cpp
@@ -5,20 +5,6 @@ void helper_handle_pre_shell_execution(std::string cmd, EVENT_ID &eventId) {
     eventId = EVENT_PRE_SHELL_EXECUTED;
 }
 
-std::string get_command(HashTable* ht) {
-    if (!ht || zend_hash_num_elements(ht) == 0) {
-        return "";
-    }
-
-    zend_hash_internal_pointer_reset(ht);
-    
-    zval* command = zend_hash_get_current_data(ht);
-    if (Z_TYPE_P(command) != IS_STRING) {
-        return "";
-    }
-    return std::string(Z_STRVAL_P(command), Z_STRLEN_P(command));
-}
-
 AIKIDO_HANDLER_FUNCTION(handle_shell_execution) {
     zend_string *cmd = NULL;
 
@@ -50,17 +36,4 @@ AIKIDO_HANDLER_FUNCTION(handle_shell_execution_with_array) {
         }
         helper_handle_pre_shell_execution(ZSTR_VAL(cmdStr), eventId);
     }
-#if PHP_VERSION_ID >= 70400
-    else if (Z_TYPE_P(cmdVal) == IS_ARRAY) {
-        HashTable* cmdTokens = Z_ARRVAL_P(cmdVal);
-        if (!cmdTokens) {
-            return;
-        }
-        std::string cmdString = get_command(cmdTokens);
-        if (cmdString.empty()) {
-            return;
-        }
-        helper_handle_pre_shell_execution(cmdString, eventId);    
-    }
-#endif
 }
