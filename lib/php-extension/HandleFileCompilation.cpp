25ac7f8778f207c63e7e1fcef00a478d51215bfa
Tudor TIMCU <tudor@aikido.dev>
2024-12-18 22:35:21 +0000

    Upgrades

--- lib/php-extension/HandleFileCompilation.cpp
+++ lib/php-extension/HandleFileCompilation.cpp
@@ -1,7 +1,7 @@
 #include "Includes.h"
 
 zend_op_array* handle_file_compilation(zend_file_handle* file_handle, int type) {
-    AIKIDO_LOG_INFO("\"zend_compile_file\" called for \"%s\"!\n", file_handle->filename);
+    AIKIDO_LOG_DEBUG("\"zend_compile_file\" called for \"%s\"!\n", file_handle->filename);
 
     eventCache.Reset();
     eventCache.functionName = "zend_compile_file";
@@ -16,6 +16,7 @@ zend_op_array* handle_file_compilation(zend_file_handle* file_handle, int type) 
 
     zend_op_array* op_array = original_file_compilation_handler(file_handle, type);
 
+    eventId = NO_EVENT_ID;
     helper_handle_post_file_path_access(eventId);
     aikido_process_event(eventId);
 
