ab13ab69f98ec3f01bb8006c5d5a6c9164622ccd
Tudor TIMCU <tudor@aikido.dev>
2024-12-18 21:11:38 +0000

    Fix

--- lib/php-extension/HandleFileCompilation.cpp
+++ lib/php-extension/HandleFileCompilation.cpp
@@ -7,7 +7,7 @@ zend_op_array* handle_file_compilation(zend_file_handle* file_handle, int type) 
     eventCache.functionName = "zend_compile_file";
 
     EVENT_ID eventId = NO_EVENT_ID;
-    helper_handle_pre_file_path_access(file_handle->filename, eventId);
+    helper_handle_pre_file_path_access((char*)file_handle->filename, eventId);
 
     if (aikido_process_event(eventId) == BLOCK) {
         // exit zend_compile_file handler and do not call the original handler, thus blocking the script file compilation
