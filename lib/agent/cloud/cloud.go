7402f28fb271812f26e3801baf8fbe02d28b2ea6
Tudor TIMCU <tudor@aikido.dev>
2025-02-17 17:41:03 +0000

    Already initialized

--- lib/agent/cloud/cloud.go
+++ lib/agent/cloud/cloud.go
@@ -21,8 +21,6 @@ func Init() {
 
 	globals.StatsData.StartedAt = utils.GetTime()
 	globals.StatsData.MonitoredSinkTimings = make(map[string]MonitoredSinkTimings)
-	globals.MiddlewareInstalled = 0
-	globals.GotTraffic = 0
 }
 
 func Uninit() {
