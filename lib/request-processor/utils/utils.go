b4d7081f6e679c46e668a0bc8d147c61a20a2940
nadiraikido <166383531+nadiraikido@users.noreply.github.com>
2025-02-20 14:55:38 +0100

    Clarify

--- lib/request-processor/utils/utils.go
+++ lib/request-processor/utils/utils.go
@@ -53,10 +53,10 @@ func ParseFormData(data string, separator string) map[string]interface{} {
 			continue
 		}
 
-		// As seen on https://github.com/php/php-src/blob/master/main/php_variables.c#L313, PHP ignores duplicate cookie names per rfc2965
+		// See: https://github.com/php/php-src/blob/master/main/php_variables.c#L313. PHP ignores duplicate cookie names per rfc2965
 		// If the user supplies 2 cookies with the same key, we should not overwrite it to ensure our parsing is similar to PHP
-		// Although form and query parameters could potentially support ; as a separator, we assume that the user wants to parse a cookie
-		// when passing ;
+		// Although form and query parameters could potentially support ; as a separator, we assume that the user of this function
+		// wants to parse a cookie.
 		if separator == ";" && KeyExists(result, keyValue[0]) {
 			continue
 		}
