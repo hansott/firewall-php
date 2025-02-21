14e613e93a221dd45118e0bc1640b00146627aad
Tudor TIMCU <tudor@aikido.dev>
2025-02-21 11:50:12 +0000

    1.0.111

--- lib/agent/globals/constants.go
+++ lib/agent/globals/constants.go
@@ -1,7 +1,7 @@
 package globals
 
 const (
-	Version                             = "1.0.110"
+	Version                             = "1.0.111"
 	ConfigUpdatedAtMethod               = "GET"
 	ConfigUpdatedAtAPI                  = "/config"
 	ConfigAPIMethod                     = "GET"
